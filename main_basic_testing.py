34ddaee27b3d129c2c82d45ab708d57f76ed426a
Kok Wei <52521979+kwcckw@users.noreply.github.com>
2022-10-31 17:03:27 +0800

    Update to upload version.

---
+++ main_basic_testing.py
@@ -0,0 +1,13 @@
+from fastapi import File, UploadFile,FastAPI, Request, Response
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/get")
+async def get_name(name:str):
+    return {"name": name}
