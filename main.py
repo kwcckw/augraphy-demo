b1fdd6ced6a7def1a9837e206d25625e93780240
Kok Wei <52521979+kwcckw@users.noreply.github.com>
2022-10-31 18:17:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ app = FastAPI()
 
 
 @app.get("/")
-async def root():
+async def root2():
     return {"message": "Hello World"}
 
 @app.get("/get")
 async def get_name(name:str):
     return {"name": name}
+    
+@app.post("/get2")
+async def get_number(number:int):
+    return {"number": number*2}
