a377aa7424a5c1b477b2b8de316990d34fcf39f0
Kok Wei <52521979+kwcckw@users.noreply.github.com>
2022-10-31 18:31:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,3 +15,8 @@ async def get_name(name:str):
 @app.post("/get2")
 async def get_number(number:int):
     return {"number": number*2}
+    
+    
+@app.post("/get3")
+async def get_name2(name2:str):
+    return {"name": name2}
