ecda5108e80b3e27932fba9e0da80a0cf594e222
Kok Wei <52521979+kwcckw@users.noreply.github.com>
2022-11-02 11:32:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,54 @@
 from fastapi import File, UploadFile,FastAPI
 from fastapi.responses import FileResponse
-import numpy as np
-import cv2
 from augraphy import *
+import cv2
+import numpy as np
+import os
 
 app = FastAPI()
 
 
 @app.post("/")
 async def upload(file: UploadFile = File(...)):
-    message = "Done!"
+    message = "Augmentation done!"
     out_response = None
     read_status  = 1
+    current_path = os.path.abspath(os.getcwd()) + "/"
     
     try:
          contents = await file.read()
-         np_array = np.fromstring(contents, np.uint8)
-         
-         
-         # augmented_image = open('augmented_image.png', 'wb')
-         # augmented_image.write(base64.b64decode((np_array)))
-         # augmented_image.close()                
-         
-         # image = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
-         # cv2.imwrite("augmented_image.png", image)
-         # out_response = FileResponse("augmented_image.png")
     except:
         message = "There was an error in uploading the file."
         read_status = 0
-
+   
+    if read_status:
+        try:
+            augmented_image = augment_image(contents)
+            out_response = FileResponse(current_path+ "augmented_image.png")
+        except Exception:
+            message = Exception #"Invalid file type!"   
         
     return out_response if out_response is not None else message
+
+
+
+def augment_image(contents: str):
+    current_path = os.path.abspath(os.getcwd()) + "/"
+    np_array = np.fromstring(contents, np.uint8)
+    image = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+    augmented_image = image
+    
+    cv2.imwrite(current_path+"input_image.png", image)
+    
+    ink_phase   = [InkBleed(p=1)]
+    paper_phase = [DirtyRollers(p=1)]
+    post_phase  = [WaterMark(p=1)]
+    pipeline    = AugraphyPipeline(ink_phase, paper_phase, post_phase)
+    
+    data_output = pipeline.augment(image)
+    augmented_image = data_output["output"]
+    
+    
+    cv2.imwrite(current_path+"augmented_image.png", augmented_image)
+
+    return augmented_image
