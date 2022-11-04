9df6c52b57ed76e432b4c29a70bafc2f8ae2570a
Kok Wei <52521979+kwcckw@users.noreply.github.com>
2022-11-04 12:37:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,41 @@ async def upload(file: UploadFile = File(...), file_name: str = Form(...)):
     # read successfully, proceed with augmentation
     if read_status:
         try:
-            augmented_image = augment_image(image)     
+            augmented_image = augment_image_basic(image)     
+            _, img_bytes = cv2.imencode(".png", augmented_image)
+            out_response = StreamingResponse(io.BytesIO(img_bytes.tobytes()), media_type="image/png")
+        except Exception:
+            message = Exception #"Invalid file type!"   
+        
+    return out_response if out_response is not None else message
+
+@app.post("/augment_default")
+async def upload(file: UploadFile = File(...), file_name: str = Form(...)):
+    message = "Augmentation done!"
+    out_response = None
+    read_status  = 1
+
+    try:
+        filepath = os.path.join('./', os.path.basename(file.filename))
+        async with aiofiles.open(filepath, 'wb') as f:
+            while chunk := await file.read(CHUNK_SIZE):
+                await f.write(chunk)
+        image = cv2.imread(filepath)
+        
+    except Exception:
+        read_status = 0
+        message = "There was an error uploading the file"
+        
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
+            detail='There was an error uploading the file')
+
+    finally:
+        await file.close()
+
+    # read successfully, proceed with augmentation
+    if read_status:
+        try:
+            augmented_image = augment_image_default(image)     
             _, img_bytes = cv2.imencode(".png", augmented_image)
             out_response = StreamingResponse(io.BytesIO(img_bytes.tobytes()), media_type="image/png")
         except Exception:
@@ -47,8 +81,7 @@ async def upload(file: UploadFile = File(...), file_name: str = Form(...)):
     return out_response if out_response is not None else message
 
 
-
-def augment_image(image):
+def augment_image_basic(image):
 
     ink_phase   = [InkBleed(p=1)]
     paper_phase = [DirtyRollers(p=1)]
@@ -59,3 +92,12 @@ def augment_image(image):
     augmented_image = data_output["output"]
 
     return augmented_image
+
+
+def augment_image_default(image):
+    
+    data_output = default_augment(image)
+    augmented_image = data_output["output"]
+
+    return augmented_image
+
