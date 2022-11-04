7894f851a6ddcd80f66fb955a69b2ffa15d2a550
Kok Wei <52521979+kwcckw@users.noreply.github.com>
2022-11-04 11:00:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,55 +17,39 @@ async def upload(file: UploadFile = File(...), data: str = Form(...)):
     message = "Augmentation done!"
     out_response = None
     read_status  = 1
-    # current_path = os.path.abspath(os.getcwd()) + "/"
-    
-    
+
     try:
-        
         filepath = os.path.join('./', os.path.basename(file.filename))
         async with aiofiles.open(filepath, 'wb') as f:
             while chunk := await file.read(CHUNK_SIZE):
                 await f.write(chunk)
-
-        augmented_image = cv2.imread(filepath)
-
-        _, img_bytes = cv2.imencode(".png", augmented_image)
-        out_response = StreamingResponse(io.BytesIO(img_bytes.tobytes()), media_type="image/png")
-
-    except Exception:
+        image = cv2.imread(filepath)
         
+    except Exception:
         read_status = 0
         message = "There was an error uploading the file"
         
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
             detail='There was an error uploading the file')
+
     finally:
         await file.close()
 
-        
-   
-    # if read_status:
-    #     try:
-    #         augmented_image = augment_image(contents)
-            
-    #         _, img_bytes = cv2.imencode(".png", augmented_image)
-    #         out_response = StreamingResponse(io.BytesIO(img_bytes.tobytes()), media_type="image/png")
-    #         # out_response = FileResponse(current_path+ "augmented_image.png")
-    #     except Exception:
-    #         message = Exception #"Invalid file type!"   
+    # read successfully, proceed with augmentation
+    if read_status:
+        try:
+            augmented_image = augment_image(image)     
+            _, img_bytes = cv2.imencode(".png", augmented_image)
+            out_response = StreamingResponse(io.BytesIO(img_bytes.tobytes()), media_type="image/png")
+        except Exception:
+            message = Exception #"Invalid file type!"   
         
     return out_response if out_response is not None else message
 
 
 
-def augment_image(contents: str):
-    # current_path = os.path.abspath(os.getcwd()) + "/"
-    np_array = np.fromstring(contents, np.uint8)
-    image = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
-    augmented_image = image
-    
-    # cv2.imwrite(current_path+"input_image.png", image)
-    
+def augment_image(image):
+
     ink_phase   = [InkBleed(p=1)]
     paper_phase = [DirtyRollers(p=1)]
     post_phase  = [WaterMark(p=1)]
@@ -73,8 +57,5 @@ def augment_image(contents: str):
     
     data_output = pipeline.augment(image)
     augmented_image = data_output["output"]
-    
-    
-    # cv2.imwrite(current_path+"augmented_image.png", augmented_image)
 
     return augmented_image
